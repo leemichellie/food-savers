3024fa57f38192906c87c1d204278b5fa0775771
leemichellie <77034959+leemichellie@users.noreply.github.com>
2021-01-17 18:28:01 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Profile from './components/pages/Profile';
+import SignOut from './components/pages/SignOut';
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/profile' component={Profile} />
+          <Route path='/sign-out' component={SignOut} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
